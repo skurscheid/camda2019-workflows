90bb7e65d183de331fb7eb7e179b4f011d9ed406
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2020-02-25 12:59:07 +1100

    adds new qsub submission scripts

--- scripts/cli_snakemake.sh
+++ scripts/cli_snakemake.sh
@@ -1,7 +1,7 @@
 /home/150/sxk150/miniconda3/envs/snakemake/bin/snakemake -s /home/150/sxk150/camda2019-workflows/Snakefile ${1}\
     --configfile /home/150/sxk150/camda2019-workflows/config.yaml\
 	--use-conda\
-	-d /scratch/kv78/camda2019-tcga\
+	-d /scratch/kv78/camda-tcga\
 	--rerun-incomplete \
         --local-cores 1\
 	--cluster-config /home/150/sxk150/camda2019-workflows/cluster.json\
